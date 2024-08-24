2150d07e887387b35d1b11921b9fd21d0fb9958a
ChakrabortyDiya <crs.diya@gmail.com>
2024-08-24 19:24:38 +0530

    sidebar items added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CallIcon from "@mui/icons-material/Call";
+import PrivacyTipIcon from "@mui/icons-material/PrivacyTip";
+import Link from "next/link";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useRouter } from "next/router";
 
-const Sidebar = () => {
-  return <div className="w-[20%] border-r-2 border-dark h-screen">Sidebar</div>;
+const SidebarItem = ({ name, Icon, classes, active, path = "/" }) => {
+  // console.log(name, active);
+
+  return (
+    <Link
+      href={path}
+      className={`${classes} text-lg flex items-center w-full my-2 h-12  pl-5 rounded-l-full cursor-pointer ${
+        active && active === name && "bg-acc"
+      }`}
+    >
+      <Icon className="mr-3" />
+      <div className="w-[80%]">{name}</div>
+    </Link>
+  );
 };
-
-export default Sidebar;
