ad08e4ce4ec87eb7f6f38e926da8bb9c5b521917
ChakrabortyDiya <crs.diya@gmail.com>
2024-08-24 22:58:42 +0530

    navbar added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container mx-auto py-4'>
+        <div className="flex justify-between items-center mx-6 gap-5">
+            <img src="/assets/logo.svg" alt="logo" />
+            <div className="flex justify-center lg:justify-between items-center gap-5">
+                <a href='#services'>Our Services</a>
+                <a href='#team'>Our team </a>
+                <a href='#cta'>Contact us</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
