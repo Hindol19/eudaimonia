47206d18e99c1d7e8c5555c13570e2e0b2dd8f76
ChakrabortyDiya <crs.diya@gmail.com>
2024-08-25 00:21:01 +0530

    services added

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -4,7 +4,7 @@ const Services = () => {
   return (
     <div className="container mx-auto py-20" id='services'>
         <p className="text-base lg:text-xl font-medium text-gray-500 text-center">
-            WHY CHOOSE websitename
+            Why choose EUDAIMONIA
         </p>
         <p className="text-3xl lg:text-5xl font-semibold text-gray-500 text-center mt-3">
             Our Services
