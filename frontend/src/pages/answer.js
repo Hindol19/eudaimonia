474578aa6bc377cb6f0de953faf635ddb38cfcdb
ChakrabortyDiya <crs.diya@gmail.com>
2024-08-24 20:04:48 +0530

    minor commit

---
+++ frontend/src/pages/answer.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Header from "@/components/Header";
+import axios from "axios";
+import QuestionCard from "@/components/QuestionCard";
