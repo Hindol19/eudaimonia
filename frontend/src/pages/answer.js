aa5ad641d2bcc5345be6077367384213fde0a327
ChakrabortyDiya <crs.diya@gmail.com>
2024-08-24 20:06:29 +0530

    answer element added

--- frontend/src/pages/answer.js
+++ frontend/src/pages/answer.js
@@ -3,3 +3,8 @@ import { useRouter } from "next/router";
 import Header from "@/components/Header";
 import axios from "axios";
 import QuestionCard from "@/components/QuestionCard";
+
+const answer = () => {
+    const [activeCategory, setActiveCategory] = useState("All");
+    const [userDetails, setUserDetails] = useState([]);
+  
