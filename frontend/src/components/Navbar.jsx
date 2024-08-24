49e4f78b71f2a24cbeeb8d8482b1b67d9081569d
ChakrabortyDiya <crs.diya@gmail.com>
2024-08-25 03:11:07 +0530

    minor changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Navbar = () => {
-  return (
-    <div className='container mx-auto py-4'>
-        <div className="flex justify-between items-center mx-6 gap-5">
-            <img src="/assets/logo.svg" alt="logo" />
-            <div className="flex justify-center lg:justify-between items-center gap-5">
-                <a href='#services'>Our Services</a>
-                <a href='#team'>Our team </a>
-                <a href='#cta'>Contact us</a>
+    return (
+        <div className='container mx-auto py-4'>
+            <div className="flex justify-between items-center mx-6 gap-5">
+                <img src="/assets/logo.svg" alt="logo" />
+                <div className="flex justify-center lg:justify-between items-center gap-5">
+                    <a href='#services'>Our Services</a>
+                    <a href='#team'>Our team </a>
+                    <a href='#cta'>Contact us</a>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
